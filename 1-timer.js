474e2aec68d6d75e19b39c8326068f6d138fc613
genaMV-dev <genanocheats@gmail.com>
2026-03-08 19:08:19 +0000

    Deploying to gh-pages from @ genaMV-dev/goit-js-hw-10@9f098118fe3da0a020d9e3657db6d9e4d07551e9 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,17 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as m,i as p}from"./assets/vendor-BbSUbo7J.js";const e=document.querySelector("button"),h=document.querySelector(".timer");let u,i;e.disabled=!0;m("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(s){console.log(s[0]);const a=Date.now();i=s[0].getTime(),i<a?(p.show({title:"Error",message:"Please choose a date in the future"}),e.disabled=!0):e.disabled=!1}});function v(s){const r=Math.floor(s/864e5),l=Math.floor(s%864e5/36e5),d=Math.floor(s%864e5%36e5/6e4),f=Math.floor(s%864e5%36e5%6e4/1e3);return{days:r,hours:l,minutes:d,seconds:f}}function n(s){return String(s).padStart(2,"0")}e.addEventListener("click",()=>{const s=i;u=setInterval(()=>{const a=Date.now(),o=s-a;if(o<=0){clearInterval(u),e.disabled=!1;return}const t=v(o),c=n(t.days),r=n(t.hours),l=n(t.minutes),d=n(t.seconds);h.innerHTML=`<div class="field">
-      <span class="value" data-days>${c}</span>
-      <span class="label">Days</span>
-    </div>
-    <div class="field">
-      <span class="value" data-hours>${r}</span>
-      <span class="label">Hours</span>
-    </div>
-    <div class="field">
-      <span class="value" data-minutes>${l}</span>
-      <span class="label">Minutes</span>
-    </div>
-    <div class="field">
-      <span class="value" data-seconds>${d}</span>
-      <span class="label">Seconds</span>
-    </div>`,e.disabled=!0},1e3)});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as h,i as y}from"./assets/vendor-BbSUbo7J.js";const n=document.querySelector("button"),u=document.querySelector("#datetime-picker"),b=document.querySelector("[data-days]"),M=document.querySelector("[data-hours]"),S=document.querySelector("[data-minutes]"),T=document.querySelector("[data-seconds]");let m,i;n.disabled=!0;u.disabled=!1;h("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]);const o=Date.now();i=e[0].getTime(),i<=o?(y.show({title:"Error",message:"Please choose a date in the future"}),n.disabled=!0):n.disabled=!1}});function p(e){const a=Math.floor(e/864e5),c=Math.floor(e%864e5/36e5),d=Math.floor(e%864e5%36e5/6e4),f=Math.floor(e%864e5%36e5%6e4/1e3);return{days:a,hours:c,minutes:d,seconds:f}}function s(e){return String(e).padStart(2,"0")}n.addEventListener("click",()=>{const e=i;m=setInterval(()=>{const o=Date.now(),r=e-o;if(r<=0){clearInterval(m),u.disabled=!1;return}n.disabled=!0,u.disabled=!0;const t=p(r),l=s(t.days),a=s(t.hours),c=s(t.minutes),d=s(t.seconds);b.innerHTML=l,M.innerHTML=a,S.innerHTML=c,T.innerHTML=d},1e3)});
 //# sourceMappingURL=1-timer.js.map
